91b865c4d421aee533a509a2b2652c7a5b3c9387
Sanqa <48105185+sanqazz@users.noreply.github.com>
2019-03-02 09:07:29 +0200

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ var myImage = document.querySelector('img');
 
 myImage.onclick = function() {
     var mySrc = myImage.getAttribute('src');
-    if(mySrc === 'images/firefox-icon.png') {
+    if(mySrc === 'firefox-icon.png') {
       myImage.setAttribute ('src','wow-icon.png');
     } else {
       myImage.setAttribute ('src','firefox-icon.png');
